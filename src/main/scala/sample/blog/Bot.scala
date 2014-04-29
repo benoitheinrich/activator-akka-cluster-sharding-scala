9f20b88133e38e6ea40055ec629f905d53417637
Benoit Heinrich <benoit.heinrich@vocado.com>
2014-04-29 19:04:00 +0200

    Updated Bot to use multiple authors for Post

--- src/main/scala/sample/blog/Bot.scala
+++ src/main/scala/sample/blog/Bot.scala
@@ -6,15 +6,20 @@ import akka.actor.Actor
 import akka.actor.ActorLogging
 import akka.cluster.Cluster
 import akka.contrib.pattern.ClusterSharding
+import scala.util.Random
 
 object Bot {
+
   private case object Tick
+
 }
 
 class Bot extends Actor with ActorLogging {
+
   import Bot._
   import context.dispatcher
-  val tickTask = context.system.scheduler.schedule(3.seconds, 3.seconds, self, Tick)
+
+  val tickTask = context.system.scheduler.schedule(3.seconds, 200.milliseconds, self, Tick)
 
   val postRegion = ClusterSharding(context.system).shardRegion(Post.shardName)
   val listingsRegion = ClusterSharding(context.system).shardRegion(AuthorListing.shardName)
@@ -27,8 +32,15 @@ class Bot extends Actor with ActorLogging {
   }
 
   var n = 0
-  val authors = Map(0 -> "Patrik", 1 -> "Martin", 2 -> "Roland", 3 -> "Björn", 4 -> "Endre")
-  def currentAuthor = authors(n % authors.size)
+  val authors = for {
+    a <- List("Patrik", "Martin", "Roland", "Björn", "Endre")
+    i <- 1 to 100
+  } yield a + i
+
+  def currentAuthors = {
+    val numberOfAuthors = Random.nextInt(4) + 1
+    Random.shuffle(authors).take(numberOfAuthors).toList
+  }
 
   def receive = create
 
@@ -36,29 +48,31 @@ class Bot extends Actor with ActorLogging {
     case Tick =>
       val postId = UUID.randomUUID().toString
       n += 1
-      val title = s"Post $n from $from"
-      postRegion ! Post.AddPost(postId, Post.PostContent(List(currentAuthor), title, "..."))
-      context.become(edit(postId))
+      val postAuthors = currentAuthors
+      val title = s"Post $n from node $from, with authors $postAuthors"
+      postRegion ! Post.AddPost(postId, Post.PostContent(postAuthors, title, "..."))
+      context.become(edit(postId, postAuthors))
   }
 
-  def edit(postId: String): Receive = {
+  def edit(postId: String, postAuthors: List[String]): Receive = {
     case Tick =>
       postRegion ! Post.ChangeBody(postId, "Something very interesting ...")
-      context.become(publish(postId))
+      context.become(publish(postId, postAuthors))
   }
 
-  def publish(postId: String): Receive = {
+  def publish(postId: String, postAuthors: List[String]): Receive = {
     case Tick =>
       postRegion ! Post.Publish(postId)
-      context.become(list)
+      context.become(list(postAuthors))
   }
 
-  val list: Receive = {
+  def list(postAuthors: List[String]): Receive = {
     case Tick =>
-      listingsRegion ! AuthorListing.GetPosts(currentAuthor)
-    case AuthorListing.Posts(summaries) =>
-      log.info("Posts by {}: {}", currentAuthor, summaries.map(_.title).mkString("\n\t", "\n\t", ""))
-      context.become(create)
-  }
+      listingsRegion ! AuthorListing.GetPosts(postAuthors.head)
 
+    case AuthorListing.Posts(summaries) =>
+      log.info("Posts by {}: {}", postAuthors.head, summaries.map(_.title).mkString("\n\t", "\n\t", ""))
+      if (postAuthors.tail.isEmpty) context.become(create)
+      else context.become(list(postAuthors.tail))
+  }
 }
--- src/test/scala/sample/blog/PostTest.scala
+++ src/test/scala/sample/blog/PostTest.scala
@@ -11,17 +11,7 @@ class PostTest extends TestKit(ActorSystem("ClusterSystem")) with FunSuiteLike w
 
   test("test post") {
     val authorListing = TestProbe()
-    /*
-        ClusterSharding(system).start(
-          typeName = Post.shardName,
-          entryProps = Some(Post.props(authorListing.ref)),
-          idExtractor = Post.idExtractor,
-          shardResolver = Post.shardResolver)
-    */
-
-    //val postActor = TestActorRef(Post.props(authorListing.ref))
     val postActor = system.actorOf(Post.props(authorListing.ref))
-    //val postActor = ClusterSharding(system).shardRegion(Post.shardName)
     println(s"postActor=$postActor")
     postActor ! Post.AddPost("12345", PostContent(List("benoit"), "title", "content"))
     postActor ! Post.GetContent("12345")
@@ -29,7 +19,7 @@ class PostTest extends TestKit(ActorSystem("ClusterSystem")) with FunSuiteLike w
     expectMsg(PostContent(List("benoit"), "title", "content"))
   }
 
-  test("test publish") {
+  test("test publish to a single author") {
     val authorListing = TestProbe()
     val postActor = system.actorOf(Post.props(authorListing.ref))
     println(s"postActor=$postActor")
@@ -42,5 +32,26 @@ class PostTest extends TestKit(ActorSystem("ClusterSystem")) with FunSuiteLike w
         s
     }
     cp should be(PostSummary("benoit", "12345", "title"))
+  }
+
+  test("test publish to two authors") {
+    val authorListing = TestProbe()
+    val postActor = system.actorOf(Post.props(authorListing.ref))
+    println(s"postActor=$postActor")
+    postActor ! Post.AddPost("12345", PostContent(List("benoit", "fred"), "title", "content"))
+    postActor ! Post.Publish("12345")
+
+    val cp1 = authorListing.expectMsgPF() {
+      case p@ConfirmablePersistent(s: PostSummary, _, _) =>
+        p.confirm()
+        s
+    }
+    val cp2 = authorListing.expectMsgPF() {
+      case p@ConfirmablePersistent(s: PostSummary, _, _) =>
+        p.confirm()
+        s
+    }
+    cp1 should be(PostSummary("benoit", "12345", "title"))
+    cp2 should be(PostSummary("fred", "12345", "title"))
   }
 }
